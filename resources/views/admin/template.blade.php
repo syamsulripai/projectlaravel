5f9757314f40ff85356311abb6b4a9ea91ba396d
M. Syamsul Ripai <syamsulrifaiaja@gmail.com>
2026-06-27 16:59:32 +0700

    test

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -185,7 +185,6 @@
     <!-- JS Bootstrap CDN -->
 <script src="https://jsdelivr.net"></script>
 
-
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 
 <script src="https://cdn.datatables.net/2.3.8/js/dataTables.js"></script>
