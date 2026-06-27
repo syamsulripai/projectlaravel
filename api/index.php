7d9ce4092f97afb73f3d7e9f35de0020873cb99d
M. Syamsul Ripai <syamsulrifaiaja@gmail.com>
2026-06-27 15:48:00 +0700

    update config vercel

--- api/index.php
+++ api/index.php
@@ -2,4 +2,19 @@
 
 // echo "HALLO PHP DARI RUN";
 
+// Buat struktur folder temporary untuk Laravel di Vercel
+$directories = [
+    '/tmp/storage/robot',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($directories as $directory) {
+    if (!is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
